07d7bf1e299dae902f1b8cbf1fd6554d01f71c8b
aman . <amangoad546@gmail.com>
2024-10-29 15:27:19 +0530

    footer update

--- src/components/utils/Footer.jsx
+++ src/components/utils/Footer.jsx
@@ -23,17 +23,14 @@ const Footer = () => {
             <div>
               <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
               <ul className="space-y-2 text-gray-200">
-<<<<<<< HEAD
                 <li>Shop No. 913, Near Milan Restaurant</li>
                 <li>Sector 10, Faridabad</li>
                 <li>Phone: 9315005234</li>
                 <li>Email: info@glamvibesalon.com</li>
-=======
                 <li><a href="https://maps.app.goo.gl/akTzqzc6NwfAhqds6" target="_blank" rel="noopener noreferrer" className="hover:text-custom-accent transition duration-300">Shop No. 913, Near Milan Restaurant</a></li>
                 <li><a href="https://maps.app.goo.gl/akTzqzc6NwfAhqds6" target="_blank" rel="noopener noreferrer" className="hover:text-custom-accent transition duration-300">Sector 10, Faridabad</a></li>
                 <li><a href="https://wa.me/919315005234" target="_blank" className="hover:text-custom-accent transition duration-300">Phone: 9315005234</a>, <a href="https://wa.me/918368509568" target="_blank" className="hover:text-custom-accent transition duration-300">8368509568</a></li>
                 <li><a href="mailto:glamevibesaloon@gmail.com" className="hover:text-custom-accent transition duration-300">Email: glamevibesaloon@gmail.com</a></li>
->>>>>>> 54cf85c9bc3866c52ca81065a07a645f6f83e85b
               </ul>
             </div>
             <div>
